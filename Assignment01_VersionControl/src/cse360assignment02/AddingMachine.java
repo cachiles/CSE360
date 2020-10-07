aa747841b87feb129fd6e3c31007f739d7a2df9c
cachiles <cachiles@gmail.com>
2020-10-06 23:26:20 -0700

    Complete Project
    Moving IDE workspace into local repo for development ease.

--- Assignment01_VersionControl/src/cse360assignment02/AddingMachine.java
+++ Assignment01_VersionControl/src/cse360assignment02/AddingMachine.java
@@ -13,41 +13,48 @@ package cse360assignment02;
 public class AddingMachine 
 {
   private int total;
+  private String transactionHistory;
   
   // 
   public AddingMachine () 
   {
     total = 0;  // not needed - included for clarity
+    transactionHistory = "0";
   }
   
   // The getTotal method should return the current total
   public int getTotal () 
   {
-    return 0;
+    return total;
   }
   
   // The add method should add the parameter to the total variable
   public void add (int value) 
   {
-
+    total += value;
+    transactionHistory += " + " + value;
   }
 
   // The subtract method should subtract the parameter from the total variable
   public void subtract (int value) 
   {
-
+    total -= value;
+    transactionHistory += " - " + value;
   }
 
   // A history of the transactions must be kept to be returned by the toString 
   // method. The history should start from the initial 0 value to the last input.
   public String toString () 
   {
-    return "";
+    // transactionHistory += " = " + total;
+    return transactionHistory;
   }
 
   // The clear method should clear your AddingMachine.
   public void clear() 
   {
-
+	total = 0;
+	transactionHistory = "0";
   }
 }
+
